58972938d760b74cfb8cd3474e83712e4f5a25d4
José Francisco Marques Neto <netomarques96@gmail.com>
2024-06-08 17:14:04 -0300

    Exemplo de uso de Predicate

---
+++ src/Predicate/PredicateExample.java
@@ -0,0 +1,17 @@
+package Predicate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateExample {
+
+    public static void main(String[] args) {
+        List<String> palavras = Arrays.asList("java" , "kotlin", "python", "javascript", "c", "go", "ruby");
+
+        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+
+
+        palavras.stream().filter(x -> x.length() > 5).forEach(System.out::println);
+    }
+}
