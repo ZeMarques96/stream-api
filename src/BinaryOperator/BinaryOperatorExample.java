fb94bb9fe229e625528c2733951a40cd10f7a9de
José Francisco Marques Neto <netomarques96@gmail.com>
2024-06-08 18:17:24 -0300

    Binary operator

---
+++ src/BinaryOperator/BinaryOperatorExample.java
@@ -0,0 +1,17 @@
+package BinaryOperator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BinaryOperatorExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+        
+        int resultado = numeros.stream().reduce(10, (x, y) -> x + y);
+
+        System.out.println();
+        System.out.println(resultado);
+    }
+}
